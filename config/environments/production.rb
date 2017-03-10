0adce4b3fc291899647ef9b101f5021aa57daf88
Ion Biziiac <ibiziiac@invoca.com>
2017-03-10 03:54:10 -0800

    Configure actionmailer in production with gmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,13 +57,21 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "blog_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
+  # config.action_mailer.smtp_settings = {
+  #   :port           => ENV['MAILGUN_SMTP_PORT'],
+  #   :address        => ENV['MAILGUN_SMTP_SERVER'],
+  #   :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+  #   :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+  #   :domain         => ENV['MAILGUN_DOMAIN'],
+  #   :authentication => :plain
+  # }
   config.action_mailer.smtp_settings = {
-    :port           => ENV['MAILGUN_SMTP_PORT'],
-    :address        => ENV['MAILGUN_SMTP_SERVER'],
-    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
-    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => ENV['MAILGUN_DOMAIN'],
-    :authentication => :plain
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :user_name            => ENV['gmail_username'],
+    :password             => ENV['gmail_password'],
+    :authentication       => "plain",
+    :enable_starttls_auto => true
   }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { :host => 'obscure-ocean-69606.herokuapp.com' }
